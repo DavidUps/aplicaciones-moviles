bf5e112bfcb87da3f0228069a1f75e12db7599fe
DavidUps <davidarribas.ciclos@gmail.com>
2017-10-20 14:33:08 +0200

    tablas c#

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tabla = { 12, 23, 34, 5 };
+            
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("posicion de la tabla " + i + " es: " + tabla[i]);
+            }
+            //te ahorras el for.
+            foreach(int numero in tabla)
+            {
+                Console.WriteLine(numero);
+            }
+            if((3.0 / 7) is System.Int32)
+            {
+                Console.WriteLine("Es de tipo entero");
+            }
+            else
+            {
+                Console.WriteLine("No es de tipo entero");
+            }
+            Console.ReadKey();
+        }
+    }
+}
