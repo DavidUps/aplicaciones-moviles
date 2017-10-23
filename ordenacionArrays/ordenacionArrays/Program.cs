7e65e680bdf8eac1797b935eae151f3bc4cbc8ed
DavidUps <davidarribas.ciclos@gmail.com>
2017-10-23 14:24:56 +0200

    ordenacion

---
+++ ordenacionArrays/ordenacionArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordenacionArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = { "Pepe", "Ana", "Juan", "Alberto", "david", "Halfonso Luis"};
+            
+            for(int i=0; i<nombres.Length-1; i++)
+            {
+                for(int j=0; j<(nombres.Length-1)-i; j++)
+                {
+                    if (nombres[j].CompareTo(nombres[j+1]) > 0)
+                    {
+                        string aux;
+                        aux = nombres[j];
+                        nombres[j] = nombres[j + 1];
+                        nombres[j + 1] = aux;
+                    }
+                }
+            }
+            foreach(string nombre in nombres)
+            {
+                Console.WriteLine(nombre);
+            }
+            Console.ReadKey();
+        }
+    }
+}
