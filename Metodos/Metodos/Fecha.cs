92d7e7b4669fe0c3a6d712dd20b8e4cb67836a1a
DavidUps <davidarribas.ciclos@gmail.com>
2017-10-30 14:31:32 +0100

    Clases

---
+++ Metodos/Metodos/Fecha.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos
+{
+    class Fecha
+    {
+        //atributos
+        int dia, mes, anio;
+
+        //Constructores
+        public Fecha()
+        {
+            dia = 1;
+            mes = 1;
+            anio = 1970;
+        }
+
+        public Fecha(int d, int m, int a)
+        {
+            dia = d;
+            mes = m;
+            anio = a;
+        }
+        //fecha se corresponde a un año bisiesto.
+        public int Anio()
+        {
+            return this.anio;
+        }
+        //Aumentar en el mes en 1 y cambar de año si es 12 + 1.
+
+        //Devolver el numero de dias desde el 1 de enero del año en curso.
+
+        //Crear 3 fechas y leer los valores de las fechas por pantalla.
+        //Crea la mayor de todas las fechas.
+        
+    }
+}
---
+++ Metodos/Metodos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fecha f1 = new Fecha(30,11,2004),
+                  f2 = new Fecha(12,2,2017),
+                  f3 = new Fecha(5, 6, 2008);
+
+            int Swithc = 0;
+            do
+            {
+
+                Console.Clear();
+                Console.WriteLine("1.- Fecha se corresponde a un año bisiesto. \n" + "2.- Aumentar en el mes en 1 y cambar de año si es 12 + 1. \n" + "3.- Devolver el numero de dias desde el 1 de enero del año en curso \n" + "4.- Crear 3 fechas y leer los valores de las fechas por pantalla. Imprime la mayor de todas las fechas.\n" + "5.- Salir");
+                Swithc = Int32.Parse(Console.ReadLine());
+                switch (Swithc)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                }
+            } while (Swithc != 5);
+        }
+    }
+}
