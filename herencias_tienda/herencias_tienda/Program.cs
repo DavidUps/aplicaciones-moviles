3f83cdd56f5d74dfcc46d86ed673569c3be474cb
DavidUps <davidarribas.ciclos@gmail.com>
2018-02-02 01:12:25 +0100

    herencia

--- herencias_tienda/herencias_tienda/Program.cs
+++ herencias_tienda/herencias_tienda/Program.cs
@@ -9,6 +9,9 @@ namespace herencias_tienda
     class Program
     {
         Computer computer1 = new Computer("Lenovo", "P012", "fast", 4, 250);
+        Tablet tablet1 = new Tablet("Amazon", "fire","1020", 120, 4, 120);
+        Movile movile1 = new Movile("Iphone", "X", "ios", 250, 4, 1000);
+
 
     }
 }
